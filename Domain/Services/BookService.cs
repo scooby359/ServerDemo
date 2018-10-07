f614cd17e6c27e94babbb4b3c3b5f2d1f3dc9b53
Chris <scooby359@gmail.com>
2018-10-07 16:51:26 +0100

    Domain started

---
+++ Domain/Services/BookService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Domain.Models;
+
+namespace Server.Domain.Services
+{
+    public class BookService : IBookService
+    {
+        public Task<Book> CreateBook(Book model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> GetBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Book>> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateBook(Book model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
