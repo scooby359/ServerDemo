8661cbd0346707a8044363036decbd07f3772c76
BIG-LENOVO\Chris <scooby359@gmail.com>
2018-10-07 15:21:50 +0100

    repo done, working on repo test

---
+++ Server.Infrastrcture.Tests/RepositoryTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+using Server.Infrastructure.Entities;
+using Server.Infrastructure.Repositories;
+using Server.Infrastructure.Settings;
+
+namespace Server.Infrastructure.Tests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        private IRepository _repository;
+
+        [SetUp]
+        public void Run()
+        {
+            var settings = new BookRepositorySettings()
+            {
+                ConnectionString = "localhost:27017",
+                Collection = "Books",
+                Database = "library"
+            };
+
+            var mockSettings = new Mock<IOptions<BookRepositorySettings>>();
+            mockSettings.Setup(x => x.Value).Returns(settings);
+
+            _repository = new Repository(mockSettings.Object);
+
+        }
+
+        [Test]
+        public async Task RepoTest()
+        {
+            // Setup test book
+            var mockBookOne = new Book()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Author = "John Smith",
+                Name = "Cool Book",
+                Year = "2018"
+            };
+
+            // Act - Test insert
+            await _repository.CreateBook(mockBookOne);
+
+            // Act / Assert - Get Book
+            var result = await _repository.GetBook(mockBookOne.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(mockBookOne.Id, result.Id);
+            Assert.AreEqual(mockBookOne.Name, result.Name);
+            Assert.AreEqual(mockBookOne.Author, result.Author);
+            Assert.AreEqual(mockBookOne.Year, result.Year);
+
+            //  Create second test entity
+            var mockBookTwo = new Book()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Author = "Mel Smith",
+                Name = "Funky Book",
+                Year = "2017"
+            };
+
+            // Act - Test insert
+            await _repository.CreateBook(mockBookTwo);
+
+            // Act / Assert - Get Book
+            result = await _repository.GetBook(mockBookTwo.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(mockBookTwo.Id, result.Id);
+            Assert.AreEqual(mockBookTwo.Name, result.Name);
+            Assert.AreEqual(mockBookTwo.Author, result.Author);
+            Assert.AreEqual(mockBookTwo.Year, result.Year);
+        
+            // Act - test get all
+            var listResult = await _repository.GetBooks();
+            Assert.AreEqual(2, listResult.Count);
+            Assert.IsTrue(listResult.Contains(mockBookOne));
+            Assert.IsTrue(listResult.Contains(mockBookTwo));
+
+            // Test delete
+            await _repository.DeleteBook(mockBookOne.Id);
+            await _repository.DeleteBook(mockBookTwo.Id);
+
+            // Assert
+            listResult = await _repository.GetBooks();
+            Assert.AreEqual(0, listResult.Count);
+        }
+    }
+}
---
+++ Server.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Server.Infrastructure.Entities;
+using Server.Infrastructure.Settings;
+
+namespace Server.Infrastructure.Repositories
+{
+    public class Repository: IRepository
+    {
+        private readonly IMongoCollection<Book> _bookCollection;
+
+        public Repository(IOptions<BookRepositorySettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.Database);
+            _bookCollection = database.GetCollection<Book>(settings.Value.Collection);
+        }
+
+        public async Task CreateBook(Book book)
+        {
+            await _bookCollection.InsertOneAsync(book);
+        }
+
+        public async Task<Book> GetBook(string id)
+        {
+            var filter = Builders<Book>.Filter.Eq(nameof(Book.Id), id);
+            return await _bookCollection.Find(filter).FirstAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _bookCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task UpdateBook(Book book)
+        {
+            var filter = Builders<Book>.Filter.Eq(nameof(Book.Id), book.Id);
+            await _bookCollection.ReplaceOneAsync(filter, book);
+        }
+
+        public async Task DeleteBook(string id)
+        {
+            var filter = Builders<Book>.Filter.Eq(nameof(Book.Id), id);
+            await _bookCollection.DeleteOneAsync(filter);
+        }
+    }
+}
