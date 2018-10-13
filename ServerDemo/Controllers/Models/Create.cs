cb780bc1870d2037031bf0394042d939e6b55ae4
Chris Walton <scooby359@gmail.com>
2018-10-13 23:28:51 +0100

    Controller created

---
+++ ServerDemo/Controllers/Models/Create.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.API.Controllers.Models
+{
+    public class Create
+    {
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the author
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// Gets or sets the year
+        /// </summary>
+        public string Year { get; set; }
+    }
+}
