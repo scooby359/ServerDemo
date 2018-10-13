a9a86ab2108cb850b0bb84ee56f33608b4587206
Chris Walton <scooby359@gmail.com>
2018-10-13 21:02:50 +0100

    Domain service complete, tests started

--- Domain/Services/BookService.cs
+++ Domain/Services/BookService.cs
@@ -2,35 +2,140 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Server.Domain.Exceptions;
 using Server.Domain.Models;
+using Server.Infrastructure.Repositories;
+using Entity = Server.Infrastructure.Entities;
 
 namespace Server.Domain.Services
 {
+
     public class BookService : IBookService
     {
-        public Task<Book> CreateBook(Book model)
+        private readonly IRepository _repository;
+
+
+        public BookService(IRepository repository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
         }
 
-        public Task DeleteBook(string id)
+        public async Task<Book> CreateBook(Book model)
         {
-            throw new NotImplementedException();
+            var errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(model.Name))
+                errors.Add(nameof(model.Name));
+
+            if (string.IsNullOrWhiteSpace(model.Author))
+                errors.Add(nameof(model.Author));
+
+            if (string.IsNullOrWhiteSpace(model.Year))
+                errors.Add(nameof(model.Name));
+
+            if (errors.Count > 0)
+                throw new ArgumentNullException(errors.ToString());
+
+            model.Id = Guid.NewGuid().ToString();
+
+            var entity = MapModelToEntity(model);
+
+            await _repository.CreateBook(entity);
+
+            return model;
         }
 
-        public Task<Book> GetBook(string id)
+        public async Task DeleteBook(string id)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentNullException(nameof(id));
+
+            var entity = await _repository.GetBook(id);
+            if (entity == null)
+                throw new BookNotFoundException(id);
+
+            await _repository.DeleteBook(id);
         }
 
-        public Task<List<Book>> GetBooks()
+        public async Task<Book> GetBook(string id)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentNullException(nameof(id));
+
+            var entity = await _repository.GetBook(id);
+            if (entity == null)
+                throw new BookNotFoundException(id);
+
+            return MapEntityToModel(entity);
         }
 
-        public Task UpdateBook(Book model)
+        public async Task<List<Book>> GetBooks()
         {
-            throw new NotImplementedException();
+            var entities = await _repository.GetBooks();
+
+            var models = new List<Book>();
+
+            foreach (var book in entities)
+            {
+                models.Add(MapEntityToModel(book));
+            }
+
+            return models;
         }
+
+        public async Task UpdateBook(Book model)
+        {
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+
+            var errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(model.Author))
+                errors.Add(nameof(model.Author));
+            if (string.IsNullOrWhiteSpace(model.Id))
+                errors.Add(nameof(model.Id));
+            if (string.IsNullOrWhiteSpace(model.Name))
+                errors.Add(nameof(model.Name));
+            if (string.IsNullOrWhiteSpace(model.Year))
+                errors.Add(nameof(model.Year));
+
+            if (errors.Count > 0)
+                throw new ArgumentNullException(errors.ToString());
+
+            var entity = await _repository.GetBook(model.Id);
+            if (entity == null)
+                throw new BookNotFoundException(model.Id);
+
+            entity.Name = model.Name;
+            entity.Author = model.Author;
+            entity.Year = model.Year;
+
+            await _repository.UpdateBook(entity);
+        }
+
+
+        #region helpers
+
+        private Book MapEntityToModel(Entity.Book entity)
+        {
+            return new Book
+            {
+                Author = entity.Author,
+                Id = entity.Id,
+                Name = entity.Name,
+                Year = entity.Year
+            };
+        }
+
+        private Entity.Book MapModelToEntity(Book model)
+        {
+            return new Entity.Book
+            {
+                Author = model.Author,
+                Id = model.Id,
+                Name = model.Name,
+                Year = model.Year
+            };
+        }
+        #endregion
     }
 }
---
+++ Server.Domain.Tests/BookServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using NUnit;
+using NUnit.Framework;
+using Server.Domain.Models;
+using Server.Domain.Services;
+using Server.Infrastructure.Repositories;
+
+namespace Server.Domain.Tests
+{
+    [TestFixture]
+    public class BookServiceTests
+    {
+
+        #region Create
+        [Test]
+        public async Task WhenCreateBook_AndArgumentNull_ThenExceptionThrown()
+        {
+            // Arrange
+            var repo = new Mock<IRepository>();
+            var service = new BookService(repo.Object);
+
+            // Act / Assert
+            Assert.Throws<ArgumentNullException>(async () => await service.CreateBook(new Book()));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase(null)]
+        public void WhenCreateBook_AndNameInvalid_ThenExceptionThrown(string name)
+        {
+            // Arrange
+            var book = CreateBook();
+            book.Name = name;
+            var repo = new Mock<IRepository>();
+            var service = new BookService(repo.Object);
+
+            // Act / Assert
+            Assert.Throws<ArgumentNullException>(async () => await service.CreateBook(book));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase(null)]
+        public void WhenCreateBook_AndYearInvalid_ThenExceptionThrown(string year)
+        {
+            // Arrange
+            var book = CreateBook();
+            book.Year = year;
+            var repo = new Mock<IRepository>();
+            var service = new BookService(repo.Object);
+
+            // Act / Assert
+            Assert.Throws<ArgumentNullException>(async () => await service.CreateBook(book));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase(null)]
+        public void WhenCreateBook_AndAuthorInvalid_ThenExceptionThrown(string author)
+        {
+            // Arrange
+            var book = CreateBook();
+            book.Author = author;
+            var repo = new Mock<IRepository>();
+            var service = new BookService(repo.Object);
+
+            // Act / Assert
+            Assert.Throws<ArgumentNullException>(async () => await service.CreateBook(book));
+        }
+
+        [Test]
+        public async Task WhenCreateBook_AndRequestValid_ThenRepoCalled()
+        {
+            // Arrange
+            var book = CreateBook();
+            var repo = new Mock<IRepository>();
+            repo.Setup(x => x.CreateBook(It.IsAny<Infrastructure.Entities.Book>())).Returns(Task.CompletedTask);
+            var service = new BookService(repo.Object);
+
+            // Act 
+
+            var result = await service.CreateBook(book);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(book, result);
+        }
+
+        #endregion
+
+        #region Delete
+        #endregion
+
+        #region Get by ID
+        #endregion
+
+        #region Get all
+        #endregion
+
+        #region Update
+        #endregion
+
+        #region helpers
+
+        private Book CreateBook()
+        {
+            return new Book
+            {
+                Author = "Author",
+                Name = "Name",
+                Year = "2018",
+                Id = Guid.NewGuid().ToString()
+            };
+        }
+
+        #endregion
+    }
+}
